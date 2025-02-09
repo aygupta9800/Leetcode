a97d4f6abd4c088345c4f41bff26eca49e0c083f
Ayush Gupta <55319952+aygupta9800@users.noreply.github.com>
2025-02-09 13:36:57 -0800

    Update 1.2_Rotate_Image.py

--- Matrix/1.2_Rotate_Image.py
+++ Matrix/1.2_Rotate_Image.py
@@ -1,3 +1,38 @@
+#Approach 2: Reversing first across diagonal then from left to right in a row
+# Time complexity: O(number of cell), Space: O(1)
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        """
+        Rotating the n*n square matrix = 
+        1. (Transpose) First reverse the matrix around main diagonal i.e a[i][j] swaped with a[j][i]
+        2. (Reflect) Reverse matrix from left to right
+        Does twice as many reads and write then rotating around cells but same time and space complexity
+        easier to read and code and can be found custom library function 
+        """
+
+        self.transpose(matrix)
+        self.reflect(matrix)
+    
+
+    def transpose(self, matrix):
+        n = len(matrix)
+
+        for i in range(n):
+            for j in range(0, i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    def reflect(self, matrix):
+        n = len(matrix)
+
+        for i in range(n):
+            for j  in range(n//2):
+                matrix[i][j], matrix[i][n-1-j] = matrix[i][n-j-1], matrix[i][j]
+
+# Approach 1: Roatating cells in corners
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
